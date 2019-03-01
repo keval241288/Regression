dedbb6f0370896859d99f22f6f75afe53d55cc56
Kevalkumar <keval241288@gmail.com>
2019-03-01 11:59:34 +0530

    regression Production script created

---
+++ src/Live_Test/Regression.java
@@ -0,0 +1,67 @@
+package Live_Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import Live_Test.Keywords;
+import Live_Test.basic_scenario;
+
+public class Regression  {
+	public static WebDriver driver;
+	public static String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
+	
+	final static Logger logger = Logger.getLogger(Regression.class);
+
+	@Before
+	public void setUp() throws Exception {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\KevalBackup\\Kevalkumar\\Keval\\Data\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		driver =new ChromeDriver();
+		driver.manage().deleteAllCookies();  
+		driver.manage().window().maximize();
+		logger.info("\n Script Start Time \n");
+		baseUrl = "https://acceptatie.autoblox.eu/";
+		ObtainDate.startdate();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		
+	}
+
+	@Test
+	public void testUntitled() throws Throwable 
+	{
+		driver.get(baseUrl);
+		Thread.sleep(2000);
+		logger.info("Chrome browser:-> " + "open browser and open URL");
+		basic_scenario.Chrome_loginid();
+		logger.info("Chrome browser:-> " + "login done");
+		basic_scenario.open_links();
+		logger.info("Basic links are Done");
+		basic_scenario.logout();
+		logger.info("Chrome browser:-> " + "Logout done");
+		Thread.sleep(2000);
+		Keywords.closeWindow();
+		logger.info("Window is closed");
+		System.out.println("\n Script End Time \n");
+		ObtainDate.enddate();
+		logger.info("PLEASE CHECK DOWNLOAD FOLDER FOR DOWNLOADED PDF FILE");
+	}
+
+	@After
+	public void tearDown() throws Throwable {
+
+
+
+
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+}
